56eae6a5537e61e25fabd91e29798dc2ec1b8553
stersylva <stersylva@gmail.com>
2018-01-12 18:13:04 -0300

    Teste inicial

---
+++ src/teste_lti/TestLTI.java
@@ -0,0 +1,34 @@
+package teste_lti;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestLTI {
+    @Test
+    public void deveAdicionarUmUsuario() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://localhost:8080/usuarios/new");
+
+        WebElement nome = driver.findElement(By.name("usuario.nome"));
+        WebElement email = driver.findElement(By.name("usuario.email"));
+
+        nome.sendKeys("Ronaldo Luiz de Albuquerque");
+        email.sendKeys("ronaldo2009@terra.com.br");
+        nome.submit();
+
+        boolean achouNome = driver.getPageSource()
+            .contains("Ronaldo Luiz de Albuquerque");
+        boolean achouEmail = driver.getPageSource()
+            .contains("ronaldo2009@terra.com.br");
+
+        assertTrue(achouNome);
+        assertTrue(achouEmail);
+
+        driver.close();
+    }
+}
